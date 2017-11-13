45f4b334b997e10f546cbe279e6bafe25c30c0a3
Ian Effendi <effendiian@gmail.com>
2017-11-13 03:59:10 -0500

    Begin of Input system.

---
+++ TRSS/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,30 @@
+﻿/*************************************************
+ * InputManager.cs
+ * 
+ * This file contains:
+ * - The InputManager class.
+ * ***********************************************/
+
+/////////////////////
+// Using statements.
+/////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TRSS.Input
+{
+    
+    /// <summary>
+    /// The static input manager class will build out control schemes for development.
+    /// </summary>
+    public static class InputManager
+    {
+
+        // Creates control schemes, mapping inputs to response types.
+
+
+
+
+    }
+}
