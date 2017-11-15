6f286b87d1e7975904f3465a506a946ca7248801
cjb5670 <cjb5760@g.rit.edu>
2017-11-15 00:45:54 -0500

    Created simple Camera Follow Script and other scenes for menu items

---
+++ TRSS/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Vector3 thisPosition;
+	public float height;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// sets thisPosition to the location of the target + height
+		thisPosition.x = target.transform.position.x;
+		thisPosition.y = height;
+		thisPosition.z = target.transform.position.z;
+
+		// sets the actual position of the camera to stored value
+		gameObject.transform.position = thisPosition;
+	}
+}
