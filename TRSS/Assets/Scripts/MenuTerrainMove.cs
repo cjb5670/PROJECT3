79b7d9d2fc0362f8b89624f05f708324c919f7ae
cjb5670 <cjb5760@g.rit.edu>
2017-11-25 21:21:27 -0500

    MainMenu animation created

---
+++ TRSS/Assets/Scripts/MenuTerrainMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTerrainMove : MonoBehaviour
+{
+	
+	public float speed;
+	private Terrain terrain; 
+	// A splat prototype is just a texture on a terrain.
+	private SplatPrototype[] splatPrototypes;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		terrain = gameObject.GetComponent<Terrain>();
+		splatPrototypes = terrain.terrainData.splatPrototypes;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		splatPrototypes[0].tileOffset += new Vector2(-speed, 0);
+		terrain.terrainData.splatPrototypes = splatPrototypes;
+	}
+}
