f9e21b17b037cb8459c652c7b49b1ee408216144
RayJayBae <rs6571@rit.edu>
2017-12-01 14:45:37 -0500

    Revert "Merge master into branchRS"

--- TRSS/Assets/Scripts/HouseScripts/ArenaManager.cs
+++ TRSS/Assets/Scripts/HouseScripts/ArenaManager.cs
@@ -3,7 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArenaManager : MonoBehaviour {
+namespace TRSS
+{
+    public class ArenaManager : MonoBehaviour
+    {
+
+        int grid_x = 10;
+        int grid_y = 10;
+
+
+        public CreateHouses cHouse;
 
     int grid_x = 15;
     int grid_y = 10;
@@ -12,7 +21,8 @@ public class ArenaManager : MonoBehaviour {
 
     public GameObject[][] arena;
 
-    System.Random rnd = new System.Random();
+
+        System.Random rnd = new System.Random();
 
     private IEnumerator coroutine;
 
@@ -23,42 +33,43 @@ public class ArenaManager : MonoBehaviour {
         arena = new GameObject[grid_x][];
         for (int x = 0; x < grid_x; x++)
         {
-            arena[x] = new GameObject[grid_y];
-            for(int y = 0; y < grid_y; y++)
-            {
+                arena[x] = new GameObject[grid_y];
+                for (int y = 0; y < grid_y; y++)
+                {
                     GameObject ground = Resources.Load("Grass") as GameObject;
                     ground = (GameObject)Instantiate(ground, new Vector3(x * 10 - 70, .0f, y * 10 - 45), Quaternion.identity);
                 //arena[x][y] = (GameObject)Instantiate(road, new Vector3(x * 10 - 45,.5f, y * 10 - 45), Quaternion.identity);
-            }
+                }
         }
 
         float road_tiles = grid_x * grid_y * .6f;
 
-        // 0 = North, 1 = East, 2 = South, 3 = West
-        float start_edge = UnityEngine.Random.Range(0, 4);
 
-        int startRoadX, startRoadY;
+            // 0 = North, 1 = East, 2 = South, 3 = West
+            float start_edge = UnityEngine.Random.Range(0, 4);
 
-        if(start_edge < 1f)
-        {
-            startRoadX = 0;
-            startRoadY = (int)UnityEngine.Random.Range(0, grid_y - 1);
-        }
-        else if (start_edge < 2f)
-        {
-            startRoadX = (int)UnityEngine.Random.Range(0, grid_x - 1);
-            startRoadY = grid_y - 1;
-        }
-        else if (start_edge < 3f)
-        {
-            startRoadX = grid_x - 1;
-            startRoadY = (int)UnityEngine.Random.Range(0, grid_y - 1);
-        }
-        else
-        {
-            startRoadX = (int)UnityEngine.Random.Range(0, grid_x - 1);
-            startRoadY = 0;
-        }
+            int startRoadX, startRoadY;
+
+            if (start_edge < 1f)
+            {
+                startRoadX = 0;
+                startRoadY = (int)UnityEngine.Random.Range(0, grid_y - 1);
+            }
+            else if (start_edge < 2f)
+            {
+                startRoadX = (int)UnityEngine.Random.Range(0, grid_x - 1);
+                startRoadY = grid_y - 1;
+            }
+            else if (start_edge < 3f)
+            {
+                startRoadX = grid_x - 1;
+                startRoadY = (int)UnityEngine.Random.Range(0, grid_y - 1);
+            }
+            else
+            {
+                startRoadX = (int)UnityEngine.Random.Range(0, grid_x - 1);
+                startRoadY = 0;
+            }
 
         roads = (int)road_tiles;
         GameObject road = Resources.Load("Road") as GameObject;
@@ -70,11 +81,7 @@ public class ArenaManager : MonoBehaviour {
         Generate_Road_nonAsync(startRoadX, startRoadY);
         //Place_New_Road_Tile(startRoadX, startRoadY, (int)road_tiles);
 
-        //if(startRoadX < .5f)
-        //{
-        //    //x = 0;
-        //}
-    }
+            Place_New_Road_Tile(startRoadX, startRoadY, (int)road_tiles);
 
     // Update is called once per frame
     void Update () {
@@ -114,97 +121,117 @@ public class ArenaManager : MonoBehaviour {
         if(roads == 0)
         {
             //StopCoroutine(coroutine);
+
         }
 
-        int newX, newY;
+        // Update is called once per frame
+        void Update()
+        {
 
-        // 0 = North, 1 = East, 2 = South, 3 = West
-        int direction = 0;
-
-        if (x == 0 && y == 0)
-        {
-            // North or East
-            direction = rnd.Next(0, 2);
-        }
-        else if (x == grid_x -1 && y == grid_y -1)
-        {
-            // West or South
-            direction = rnd.Next(2, 4);
-        }
-        else if (x == 0 && y == grid_y -1)
-        {
-            // South or East
-            direction = rnd.Next(1, 3);
         }
 
-        else if(x == grid_x -1 && y == 0)
+        /**
+         * Recursive method that attepts to create a new road tile. If the
+         * random placement fails then it tries again. If the random placement
+         * is successful then it creates a new one with one less tile remaining
+         * to be created.
+         */
+        void Place_New_Road_Tile(int x, int y, int tiles)
         {
-            // North or West
-            direction = rnd.Next(0, 4);
+            if (tiles == 0)
+            {
+                return;
+            }
 
-            while (direction == 2 || direction == 1)
+            int newX, newY;
+
+            // 0 = North, 1 = East, 2 = South, 3 = West
+            int direction = 0;
+
+            if (x == 0 && y == 0)
+            {
+                // North or East
+                direction = rnd.Next(0, 2);
+            }
+            else if (x == grid_x - 1 && y == grid_y - 1)
+            {
+                // West or South
+                direction = rnd.Next(2, 4);
+            }
+            else if (x == 0 && y == grid_y - 1)
+            {
+                // South or East
+                direction = rnd.Next(1, 3);
+            }
+
+            else if (x == grid_x - 1 && y == 0)
+            {
+                // North or West
+                direction = rnd.Next(0, 4);
+
+                while (direction == 2 || direction == 1)
+                {
+                    direction = rnd.Next(0, 4);
+                }
+            }
+
+            else if (x == 0)
+            {
+                direction = rnd.Next(0, 3);
+            }
+
+            else if (x == grid_x - 1)
+            {
+                direction = rnd.Next(0, 4);
+                while (direction == 1)
+                {
+                    direction = rnd.Next(0, 4);
+                }
+            }
+
+            else if (y == 0)
+            {
+                direction = rnd.Next(0, 4);
+                while (direction == 2)
+                {
+                    direction = rnd.Next(0, 4);
+                }
+
+            }
+
+            else if (y == grid_y - 1)
+            {
+                direction = rnd.Next(1, 4);
+            }
+            else
             {
                 direction = rnd.Next(0, 4);
             }
-        }
 
-        else if (x == 0)
-        {
-            direction = rnd.Next(0, 3);
-        }
-
-        else if (x == grid_x - 1)
-        {
-            direction = rnd.Next(0, 4);
-            while (direction == 1)
+            // 0 = North, 1 = East, 2 = South, 3 = West
+            switch (direction)
             {
-                direction = rnd.Next(0, 4);
-            }   
-        }
-
-        else if (y == 0)
-        {
-            direction = rnd.Next(0, 4);
-            while (direction == 2)
-            {
-                direction = rnd.Next(0, 4);
+                case 0:
+                    newX = x;
+                    newY = y + 1;
+                    break;
+                case 1:
+                    newX = x + 1;
+                    newY = y;
+                    break;
+                case 2:
+                    newX = x;
+                    newY = y - 1;
+                    break;
+                case 3:
+                    newX = x - 1;
+                    newY = y;
+                    break;
+                default:
+                    newX = x;
+                    newY = y;
+                    break;
             }
-            
-        }
-
-        else if (y == grid_y - 1)
-        {
-            direction = rnd.Next(1, 4);
-        }
-        else
-        {
-            direction = rnd.Next(0, 4);
-        }
-
-        // 0 = North, 1 = East, 2 = South, 3 = West
-        switch (direction)
-        {
-            case 0:
-                newX = x;
-                newY = y + 1;
-                break;
-            case 1:
-                newX = x + 1;
-                newY = y;
-                break;
-            case 2:
-                newX = x;
-                newY = y - 1;
-                break;
-            case 3:
-                newX = x - 1;
-                newY = y;
-                break;
-            default:
-                newX = x;
-                newY = y;
-                break;
-        }
 
         this.newX = newX;
         this.newY = newY;
@@ -231,31 +258,31 @@ public class ArenaManager : MonoBehaviour {
             //Place_New_Road_Tile(newX, newY, tiles);
             //yield return Place_New_Road_Tile(newX, newY, tiles - 1);
         }
+
+        bool RoadDoesntMakeA2x2(int x, int y)
+        {
+            if (x > 0 && y > 0 && arena[x - 1][y] && arena[x - 1][y - 1] && arena[x][y - 1])
+            {
+                return false;
+            }
+            else if (x < grid_x - 1 && y > 0 && arena[x][y - 1] && arena[x + 1][y] && arena[x + 1][y - 1])
+            {
+                return false;
+            }
+            else if (x > 0 && y < grid_y - 1 && arena[x - 1][y + 1] && arena[x][y + 1] && arena[x - 1][y + 1])
+            {
+                return false;
+            }
+            else if (x < grid_x - 1 && y < grid_y - 1 && arena[x + 1][y + 1] && arena[x][y + 1] && arena[x + 1][y])
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
     }
-
-    bool RoadDoesntMakeA2x2(int x, int y)
-    {
-        if(x > 0 && y > 0 && arena[x-1][y] && arena[x-1][y-1] && arena[x][y-1])
-        {
-            return false;
-        }
-        else if(x < grid_x -1 && y > 0 && arena[x][y - 1] && arena[x + 1][y] && arena[x + 1][y - 1])
-        {
-            return false;
-        }
-        else if(x > 0 && y < grid_y - 1 && arena[x-1][y + 1] && arena[x][y + 1] && arena[x - 1][y + 1])
-        {
-            return false;
-        }
-        else if (x < grid_x - 1 && y < grid_y - 1 && arena[x + 1][y + 1] && arena[x][y + 1] && arena[x + 1][y])
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-
-    }
-
 }
---
+++ TRSS/Assets/Scripts/HouseScripts/CreateHouses.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TRSS
+{
+    public class CreateHouses : MonoBehaviour
+    {
+
+        public ArenaManager aManager;
+        GameObject[][] gObjs;
+
+        public GameObject house;
+
+        // Use this for initialization
+        void Start(ArenaManager aMan)
+        {
+
+            aManager = aMan;
+            gObjs = aManager.arena;
+            Populate();
+
+        }
+
+        void Populate()
+        {
+
+            //ground size is 100:100 )||( house size is 5:5
+
+            int rand;
+
+            for (int x = 0; x < gObjs.Length; x++)
+            {
+
+                for (int y = 0; y < gObjs[x].Length; y++)
+                {
+
+                    if (gObjs[x][y] == null)
+                    {
+
+                        rand = Random.Range(0, 10);
+                        if (rand >= 5)
+                        {
+
+                            Debugger.Print("House Created");
+                            Instantiate(house, new Vector3((x * 10) - 45, 1.97f, (y * 10) - 45), Quaternion.identity);
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+
+        }
+    }
+}
